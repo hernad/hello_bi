b4b0ab2b654d5bd1a3e94b9dd37539ccf9ef35f9
Ernad Husremovic <hernad@bring.out.ba>
2012-01-22 22:57:33 +0100

    primary key u hijerarhiji navesti !

--- create_tables_hello_bi_star.sql
+++ create_tables_hello_bi_star.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS public.dim_gradovi CASCADE;
+
 CREATE TABLE "public".dim_gradovi
 (
-  grad_id BIGSERIAL
-, grad TEXT
+  grad_id BIGSERIAL PRIMARY KEY, 
+  grad TEXT
 )
+
 ;CREATE UNIQUE INDEX idx_dim_gradovi_pk
  ON dim_gradovi
 ( 
@@ -18,9 +21,10 @@ CREATE INDEX idx_dim_gradovi_lookup
 ;
 
 
+DROP TABLE IF EXISTS public.dim_art_kategorije CASCADE;
 CREATE TABLE "public".dim_art_kategorije
 (
-  kateg_id BIGSERIAL
+  kateg_id BIGSERIAL PRIMARY KEY
 , kateg TEXT
 )
 ;CREATE UNIQUE INDEX idx_dim_art_kategorije_pk
@@ -38,9 +42,10 @@ CREATE INDEX idx_dim_art_kategorije_lookup
 ;
 
 
+DROP TABLE IF EXISTS public.dim_partneri CASCADE;
 CREATE TABLE "public".dim_partneri
 (
-  partner_id BIGSERIAL
+  partner_id BIGSERIAL PRIMARY KEY
 , partner TEXT
 )
 ;CREATE UNIQUE INDEX idx_dim_partneri_pk
@@ -58,9 +63,10 @@ CREATE INDEX idx_dim_partneri_lookup
 ;
 
 
+DROP TABLE IF EXISTS public.dim_artikli CASCADE;
 CREATE TABLE "public".dim_artikli
 (
-  artikal_id BIGSERIAL
+  artikal_id BIGSERIAL PRIMARY KEY
 , artikal TEXT
 )
 ;CREATE UNIQUE INDEX idx_dim_artikli_pk
@@ -78,9 +84,10 @@ CREATE INDEX idx_dim_artikli_lookup
 ;
 
 
+DROP TABLE IF EXISTS public.dim_cijene CASCADE;
 CREATE TABLE "public".dim_cijene
 (
-  cijena_id BIGSERIAL
+  cijena_id BIGSERIAL PRIMARY KEY
 , cijena DOUBLE PRECISION
 )
 ;CREATE UNIQUE INDEX idx_dim_cijene_pk
@@ -97,14 +104,14 @@ CREATE INDEX idx_dim_cijene_lookup
 )
 ;
 
-
+DROP TABLE IF EXISTS public.dim_time CASCADE;
 CREATE TABLE "public".dim_time
 (
-  time_id BIGSERIAL
-, "year" DOUBLE PRECISION
-, "month" DOUBLE PRECISION
-, "day" DOUBLE PRECISION
-, day_of_week DOUBLE PRECISION
+  time_id BIGSERIAL PRIMARY KEY
+, "year" int
+, "month" int
+, "day" int
+, day_of_week int
 )
 ;CREATE UNIQUE INDEX idx_dim_time_pk
  ON dim_time
@@ -123,17 +130,20 @@ CREATE INDEX idx_dim_time_lookup
 )
 ;
 
+DROP TABLE IF EXISTS public.fact_invoices CASCADE;
+
 CREATE TABLE "public".fact_invoices
 (
-  fact_id DOUBLE PRECISION
+  fact_id bigint PRIMARY KEY
 , prodaja DOUBLE PRECISION
 , kolicina DOUBLE PRECISION
-, grad_id BIGINT
-, kateg_id BIGINT
-, partner_id BIGINT
-, artikal_id BIGINT
-, cijena_id BIGINT
-, time_id BIGINT
+, grad_id BIGINT REFERENCES dim_gradovi
+, kateg_id BIGINT REFERENCES dim_art_kategorije
+, partner_id BIGINT REFERENCES dim_partneri
+, artikal_id BIGINT REFERENCES dim_artikli
+, cijena_id BIGINT REFERENCES dim_cijene
+, time_id BIGINT REFERENCES dim_time
 )
 ;
+
 
--- rsync_schema.sh
+++ rsync_schema.sh
@@ -1,2 +1,2 @@
-rsync -v schema_hello_bi.xml ~/knowhowERP/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
+rsync -v schema_hello_bi_star.xml ~/knowhowERP/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
 
