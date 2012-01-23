cb523a7779aa28e544e1973af6a1850e4466390d
Ernad Husremovic <hernad@bring.out.ba>
2012-01-23 23:56:17 +0100

    push postgresql dravjer na server

--- rsync_schema.sh
+++ rsync_schema.sh
@@ -1,2 +1,7 @@
-rsync -v schema_hello_bi_star.xml ~/knowhowERP/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
+#rsync -v schema_hello_bi_star.xml ~/knowhowERP/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
+HOST=www@fit.bring.out.ba
+ROOT_DIR=/home/www
+
+rsync -v schema_hello_bi_star.xml $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
+rsync -v postgresql-8.4-702.jdbc3.jar $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/lib/postgresql-8.4-702.jdbc3.jar
 
