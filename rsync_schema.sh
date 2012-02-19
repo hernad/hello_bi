c12e809476225f9f592b3a755a90e728514fb305
Ernad Husremovic <hernad@bring.out.ba>
2012-02-19 09:56:36 +0100

    fit olap 2 data source

--- rsync_schema.sh
+++ rsync_schema.sh
@@ -4,8 +4,14 @@ ROOT_DIR=/home/www
 
 #rsync -v postgresql-8.4-702.jdbc3.jar $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/lib/postgresql-8.4-702.jdbc3.jar
 
-rsync -v schema_hello_bi_star.xml $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
-DS=fit_olap
+#rsync -v schema_hello_bi_star.xml $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/hello_bi/schema_hello_bi.xml
+
+
+SCHEMA=fit_olap_2.xml
+rsync -v schema_hello_bi_star.xml $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/fit_olap/$SCHEMA
+
+
+DS=fit_olap_2
 rsync -v saiku-datasource_$DS $HOST:$ROOT_DIR/saiku-server/tomcat/webapps/saiku/WEB-INF/classes/saiku-datasources/$DS
 
 
